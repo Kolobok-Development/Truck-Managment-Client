29845b5d34467626a39820355a109ca835f3ec83
Nikita Savitskiy <48969303+SavniKar@users.noreply.github.com>
2021-06-17 15:07:37 -0400

    Create LoadingScreen.js

---
+++ app/screens/LoadingScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function LoadingScreen(){
+
+    return (
+        <View style={styles.container}>
+            <Text>Loading...</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
