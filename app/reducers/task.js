19b28cbefedb6b64e4222c8d2e1a456e178161f1
Nikita Savitskiy <48969303+SavniKar@users.noreply.github.com>
2021-06-18 06:18:37 -0400

    Update task.js

--- app/reducers/task.js
+++ app/reducers/task.js
@@ -21,8 +21,37 @@ export default function (state = initialState, action) {
 
     const  {type, payload} = action;
 
+    //console.log(payload);
+
     switch(type) {
+        case FETCH_TASKS_REQUEST:
+            return {
+                ...state
+            }
+        case FETCH_TASKS_SUCCESS:
+            return {
+                ...state,
+                tasks: payload
+            };
+        case FETCH_TASKS_FAIL:
+            return {
+                ...state
+            }
+
+            case FETCH_TASK_REQUEST:
+            return {
+                ...state
+            }
+        case FETCH_TASK_SUCCESS:
+            return {
+                ...state,
+                task: payload
+            };
+        case FETCH_TASK_FAIL:
+            return {
+                ...state
+            }
         default:
-            return state
+            return state;
     }
 }
