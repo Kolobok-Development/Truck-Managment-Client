f741a66815f6b268ab1b7982261c20e593ee23ee
Nikita Savitskiy <48969303+SavniKar@users.noreply.github.com>
2021-06-18 06:19:29 -0400

    Update task.js

--- app/actions/task.js
+++ app/actions/task.js
@@ -7,3 +7,61 @@ import {
     FETCH_TASKS_FAIL
 
 } from './types'
+
+import axios from 'axios';
+import configData from '../../config.json';
+import { storeData, retrieveData } from '../utils/storage';
+import { setAuthToken } from '../utils/setAuthToken';
+
+export const loadTasks = () => async dispatch => {
+    
+    dispatch({type: FETCH_TASKS_REQUEST});
+
+    const token = await retrieveData('token');
+
+    if (token !== null) {
+        setAuthToken(token);
+    } 
+    
+    try {
+        const res = await axios.get(`${configData.baseURL}/api/task/getAllTasks`);
+
+        dispatch({
+            type: FETCH_TASKS_SUCCESS,
+            payload: res.data
+        });
+
+    }catch{
+        console.log(err);
+        dispatch({
+            type: FETCH_TASKS_FAIL
+        });
+
+    }
+}
+
+export const loadTaskById = (id) => async dispatch => {
+    
+    dispatch({type: FETCH_TASK_REQUEST});
+
+    const token = await retrieveData('token');
+
+    if (token !== null) {
+        setAuthToken(token);
+    } 
+    
+    try {
+        const res = await axios.get(`${configData.baseURL}/api/task/getTaskById/${id}`);
+        console.log(res.data);
+        dispatch({
+            type: FETCH_TASK_SUCCESS,
+            payload: res.data
+        });
+
+    }catch (err){
+        dispatch({
+            type: FETCH_TASK_FAIL
+        });
+
+    }
+}
