1a0a38de50c228f35ac1c0b03c1e8d642e3afb61
Nikita Savitskiy <48969303+SavniKar@users.noreply.github.com>
2021-06-18 20:01:25 -0400

    Update HomeScreen.js

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -24,18 +24,42 @@ export default function HomeScreen( navigator ){
         dispatch(loadTasks());
     },[]);
 
+    const pushContent = (item, index) => {
+        switch (item.status.toString()) {
+            case "waiting":
+                return <TouchableOpacity 
+                    onPress={ event => { dispatch(loadTaskById(item._id)); }} 
+                    key={item.creationDate.toString()} 
+                    style={styles.buttonWaiting}> 
+                        <Text>Статус: {item.status}</Text>
+                        <Text>Механик: {item.mechanic}</Text>
+                </TouchableOpacity>
+            case "closed":
+                return <TouchableOpacity 
+                    onPress={ event => { dispatch(loadTaskById(item._id)); }} 
+                    key={item.creationDate.toString()} 
+                    style={styles.buttonClosed}> 
+                        <Text>Статус: {item.status}</Text>
+                        <Text>Механик: {item.mechanic}</Text>
+                </TouchableOpacity>
+            default:
+                return <TouchableOpacity 
+                    onPress={ event => { dispatch(loadTaskById(item._id)); }} 
+                    key={item.creationDate.toString()} 
+                    style={styles.button}> 
+                        <Text>Статус: {item.status}</Text>
+                        <Text>Механик: {item.mechanic}</Text>
+                </TouchableOpacity>
+        }
+    }
+    
+
     const Tasks = ({ show }) => {
         let content = [];
       
         if (show) {
             tasksState.tasks.map((item, index) => {
-                content.push(<TouchableOpacity 
-                                onPress={ event => { dispatch(loadTaskById(item._id)); }} 
-                                key={item.creationDate.toString()} 
-                                style={styles.button}> 
-                                    <Text>Статус: {item.status}</Text>
-                                    <Text>Механик: {item.mechanic}</Text>
-                            </TouchableOpacity>);
+                content.push(pushContent(item, index));
             });
         } else {
           content = (
@@ -77,7 +101,21 @@ const styles = StyleSheet.create({
         padding: 10
     },
     scrollView: {
-        //marginHorizontal: 20,
-      }
+    },
+    buttonWaiting: {
+        width: Dimensions.get('window').width - 50,
+        marginVertical: 10,
+        alignItems: 'center',
+        backgroundColor: 'yellow',
+        padding: 10
+    },
+    buttonClosed: {
+        width: Dimensions.get('window').width - 50,
+        marginVertical: 10,
+        alignItems: 'center',
+        backgroundColor: 'red',
+        padding: 10
+    }
 });
+
 
