e42dd32a5b8040251cc6632c7b9cfedf35801c6e
Nikita Savitskiy <48969303+SavniKar@users.noreply.github.com>
2021-06-23 16:52:53 -0400

    Update TruckScreen.js

--- app/screens/TruckScreen.js
+++ app/screens/TruckScreen.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {View, Text, StyleSheet, SafeAreaView, TouchableOpacity, ScrollView, Dimensions} from 'react-native';
 
+import Trucks from '../components/trucks/Trucks';
 
+import { loadTruckById, loadTrucks } from '../actions/truck';
+
+import { useSelector } from 'react-redux';
+
+//Redux
+import { useDispatch } from 'react-redux';
 
 export default function TruckScreen(){
 
+    const trucksState = useSelector(state => state.truck);
+
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+        dispatch(loadTrucks());
+    },[]);
+
     return (
         <View style={styles.container}>
-            <Text>Грузовики</Text>
+            {trucksState !== undefined ? (
+                <SafeAreaView style={styles.container}>
+                    <ScrollView style={styles.scrollView}>
+                        <Trucks trucksState={trucksState} dispatch={dispatch}  />
+                    </ScrollView>
+                </SafeAreaView>
+            ):(
+                <Text>Все задачи выполнены!</Text>
+            )}
         </View>
     )
 }
@@ -17,5 +40,12 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    button: {
+        width: Dimensions.get('window').width - 50,
+        marginVertical: 10,
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        padding: 10
     }
 });
