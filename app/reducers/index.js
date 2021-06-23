e872df6a31a33b251a3384bed471a1f34a7e94ce
Nikita Savitskiy <48969303+SavniKar@users.noreply.github.com>
2021-06-23 16:55:07 -0400

    Update index.js

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
 import task from './task';
+import truck from './truck';
 
 export default combineReducers({
     auth,
     task,
+    truck
 });
